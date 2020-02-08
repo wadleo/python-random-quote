03a787846d8819ecb5b3b31f8e7062b7a9e99531
Wadinga Leonard Ngonga <wadingaleonard@gmail.com>
2020-02-08 13:07:27 +0100

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
+import random
+
 def magic():
   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
 
-  print(quotes[0])
+  print(quotes[rnd])
 
 if __name__== "__main__":
   magic()
